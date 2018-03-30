e0946af6b34e1e1f7c9a791afeae68448c31c2a9
LanGnezda <36532684+LanGnezda@users.noreply.github.com>
2018-03-30 18:06:34 +0200

    Add files via upload

---
+++ Scripts/Answermechanic.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Answermechanic : MonoBehaviour {
+
+    public float range = 1000f;
+    public Transform Exitpoint;
+    public float Level;
+    private bool zeroishit = false;
+    private bool oneishit = false;
+    private bool twoishit = false;
+    private bool threeishit = false;
+    private bool fourishit = false;
+    private bool fiveishit = false;
+    private bool sixishit = false;
+    private bool sevenishit = false;
+    private bool eightishit = false;
+    private bool nineishit = false;
+    private bool YESishit = false;
+    private bool NOishit = false;
+
+    public Text LevelText;
+    public Text QuestionText;
+
+
+    // Use this for initialization
+    void Start () {
+        Level = 1;
+        QuestionText.text = "Can your skin turn pink by eating shrimps?";
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Exitpoint.position, Exitpoint.transform.right, out hit, range))
+            {
+                if (Level == 1 && hit.collider.tag == "YES")
+                {
+                    Level = 2;
+                    LevelText.text = "Level: 2";
+                    QuestionText.text = "What is 5 * 10?";
+
+                }
+
+                if (Level == 2)
+                {
+                    if (hit.collider.tag == "No5")
+                    {
+                        fiveishit = true;
+                        return;
+                    }
+                    
+                    if (fiveishit == true && hit.collider.tag == "No0")
+                    {
+                        Level = 3;
+                        Debug.Log("level 3");
+                        LevelText.text = "Level: 3";
+                        QuestionText.text = "How many in a dozen?";
+
+                        fiveishit = false;
+                    }
+
+                    else
+                    {
+                        fiveishit = false;
+                        Debug.Log("resetting");
+
+                    }
+                }
+
+                if (Level == 3)
+                {
+                    if (hit.collider.tag == "No1")
+                    {
+                        oneishit = true;
+                        Debug.Log("OneIsHit");
+                        return;
+                    }
+                    if (oneishit == true && hit.collider.tag == "No2")
+                    {
+                        Level = 4;
+                        Debug.Log("Level = 4");
+                        QuestionText.text = "Was the egg first?";
+                        oneishit = false;
+                    }
+                    else
+                    {
+                        oneishit = false;
+                        Debug.Log("resetting");
+                    }
+                }
+                if (Level == 4)
+                {
+                    if (hit.collider.tag == "YES")
+                    {
+                        Level = 5;
+                        LevelText.text = "Level: 5";
+                        QuestionText.text = "Is Lan good at coding?";
+                    }
+                }
+                if ( Level == 5)
+                {
+                    if (hit.collider.tag == "NO")
+                    {
+                        LevelText.text = "FINISHED!";
+                        QuestionText.text = "You're done now, it's true, he really isn't";
+
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Scripts/Gunscript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunscript : MonoBehaviour
+{
+    private Animator anim;
+
+    public float range = 1000f;
+    public int BulletsPerMag = 7;
+    public int CurrentBullets;
+    public Transform Exitpoint;
+    public float FireRate = 1f;
+    public float FireTimer;
+    public AudioSource Audioclip;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        CurrentBullets = BulletsPerMag;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+
+        if (FireTimer < FireRate)
+            FireTimer += Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (info.IsName("Idlefixed"))
+        {
+
+            anim.SetBool("Fire", false);
+
+        }
+   
+    }
+
+    private void Fire()
+    {
+        if (FireTimer < FireRate) return;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Exitpoint.position, Exitpoint.transform.right, out hit, range))
+        {
+            Debug.Log(hit.transform.name + "found!");
+            CurrentBullets--;
+            anim.SetBool("Fire", true);
+            anim.SetBool("Idle", false);
+        }
+        Audioclip.Play();
+        anim.SetBool("Fire", true);
+        anim.SetBool("Idle", false);
+
+
+
+        FireTimer = 0.0f;
+
+        if (CurrentBullets == 0)
+        {
+            FireRate = 3f;
+            CurrentBullets = BulletsPerMag;
+        }
+
+        else
+        {
+            FireRate = 1f;
+        }
+    }
+
+        
+
+}
+
